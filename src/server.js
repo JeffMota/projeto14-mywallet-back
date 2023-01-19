680f5a4fefc3bfc485da3316b7b4d995962a2ecc
JeffMota <jmota906@gmail.com>
2023-01-19 11:18:57 -0300

    Login, validação de dados e token

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import { MongoClient } from 'mongodb'
+import { MongoClient, ObjectId } from 'mongodb'
 import joi from 'joi'
+import bcrypt from 'bcrypt'
+import { v4 as uuid } from 'uuid'
 dotenv.config()
 
 const server = express()
@@ -21,13 +23,14 @@ try {
 const db = mongoClient.db()
 
 //Cadastrar usuário
-server.post('/cadastro', async (req, res) => {
+server.post('/sign-up', async (req, res) => {
     const body = req.body
 
     const userSchema = joi.object({
-        nome: joi.string().min(3).max(40).required(),
+        name: joi.string().min(3).max(30).required(),
         email: joi.string().email().max(30).required(),
-        senha: joi.string().min(3).max(30).required()
+        password: joi.string().min(3).max(30).required(),
+        confirmPassword: joi.string().valid(joi.ref('password')).required()
     })
 
     const { error } = userSchema.validate(body, { abortEarly: false })
@@ -37,7 +40,80 @@ server.post('/cadastro', async (req, res) => {
         return res.status(422).send(err)
     }
 
-    res.send(body)
+    try {
+
+        const userExist = await db.collection('users').findOne({ email: body.email })
+        if (userExist) return res.status(409).send('Usuário já cadastrado')
+
+        const hashedPass = bcrypt.hashSync(body.password, 10)
+
+        await db.collection('users').insertOne({ name: body.name, email: body.email, password: hashedPass })
+
+        res.sendStatus(201)
+
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+
+})
+
+//Login
+server.post('/sign-in', async (req, res) => {
+    const body = req.body
+
+    const loginSchema = joi.object({
+        email: joi.string().email().max(30).required(),
+        password: joi.string().min(3).max(30).required()
+    })
+
+    const { error } = loginSchema.validate(body, { abortEarly: false })
+    if (error) {
+        const err = error.details.map(detail => detail.message)
+        return res.status(422).send(err)
+    }
+
+    try {
+
+        const userExist = await db.collection('users').findOne({ email: body.email })
+
+        if (!userExist) return res.status(404).send('Dados incorretos!')
+
+        if (!bcrypt.compareSync(body.password, userExist.password)) return res.status(404).send('Dados incorretos!')
+
+        const alreadyExist = await db.collection('session').findOne({ _id: ObjectId(userExist._id) })
+
+        if (alreadyExist) {
+            const token = uuid()
+
+            await db.collection('session').updateOne(
+                {
+                    _id: ObjectId(userExist._id)
+                },
+                {
+                    $set: {
+                        userId: userExist._id,
+                        token
+                    }
+                }
+            )
+
+            return res.send(token)
+
+        }
+
+        const token = uuid()
+
+        await db.collection('session').insertOne({
+            userId: userExist._id,
+            token
+        })
+
+        res.send(token)
+
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+
 })
 
 const PORT = 5000
